a2b308ccf510b99bdde4b1024d5264d1225d865c
Doug Red <douglasred63@yahoo.com>
2022-12-08 14:27:27 -0600

    add dynamic tasks to taskinator

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,23 @@ const tasksToDoEl = document.querySelector("#tasks-to-do");
 const createTaskHandler = function (event) {
   event.preventDefault();
 
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  let taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  taskInfoEl.innerHTML =
+    "<h3 class='task-name'>" +
+    taskNameInput +
+    "</h3><span class='task-type'>" +
+    taskTypeInput +
+    "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
   tasksToDoEl.appendChild(listItemEl);
 };
 
